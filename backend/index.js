e4a094b5eb25cc78cc8a6c2839e55060cf95bcd3
JavaScriptGoggins <sethgoodman657@gmail.com>
2023-12-02 19:16:19 -0800

    completed assignment 4 for real this time

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
+const jwt = require('json-web-token')
 
 const cookieSession = require('cookie-session')
 
